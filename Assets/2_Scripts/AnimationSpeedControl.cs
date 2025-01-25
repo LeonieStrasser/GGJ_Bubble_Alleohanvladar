84cf519c2b1d52bd14989db9358872f913b91774
Vlad Trutniev <trutniev.v@gmail.com>
2025-01-25 16:10:35 +0100

    hand animation

---
+++ Assets/2_Scripts/AnimationSpeedControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class AnimationSpeedControl : MonoBehaviour
+{
+    // Public variable für die Geschwindigkeit, die im Editor angepasst werden kann
+    [Range(0f, 5f)]
+    public float animationSpeed = 1f;
+
+    private Animator animator;
+
+    void Start()
+    {
+        // Animator des GameObjects holen
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Die Geschwindigkeit des Animators in Echtzeit anpassen
+        if (animator != null)
+        {
+            animator.speed = animationSpeed;
+        }
+    }
+}
