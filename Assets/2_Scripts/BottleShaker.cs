3afba1a2e66d7d8aa1efd6c48a22779e0cb1db13
Darius <s0581823@htw-berlin.de>
2025-01-25 12:45:01 +0100

    Shake registration works but is still too wonky, needs refinement. Started working on game manager.

--- Assets/2_Scripts/BottleShaker.cs
+++ Assets/2_Scripts/BottleShaker.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using NaughtyAttributes;
+using UnityEngine.Serialization;
+using GGJ_Cowboys;
 
 public class BottleShaker : MonoBehaviour
 {
@@ -9,6 +14,33 @@ public class BottleShaker : MonoBehaviour
     private Vector2 
         lastC1Value = new(),
         lastC2Value = new();
+
+    private float
+        C1Velocity,
+        smoothedC1VelocityChanges,
+        C2Velocity,
+        smoothedC2VelocityChanges;
+
+    private List<float>
+        C1VelChanges = new List<float>(),
+        C2VelChanges = new List<float>();
+
+    [FormerlySerializedAs("VelocityChangeSmooting")] [SerializeField, BoxGroup("Settings")] 
+    private int VelocityChangeSmoothing = 3;
+
+    [SerializeField, BoxGroup("Thresholds")]
+    private float
+        SmallShake = 15,
+        MediumShake = 40,
+        BigShake = 60;
+
+    private ShakeState 
+        C1_CurrentShake,
+        C1_lastBiggestShake,
+        C2_CurrentShake,
+        C2_lastBiggestShake;
+
+    public Action<Cowboy, ShakeState> ShakeEvent;
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,10 +48,27 @@ public class BottleShaker : MonoBehaviour
     {
         Cowboy_1_Stick = InputSystem.actions.FindAction("Player/Cowboy_1_Stick");
         Cowboy_2_Stick = InputSystem.actions.FindAction("Player/Cowboy_2_Stick");
+
+        for (int i = 0; i < VelocityChangeSmoothing; i++)
+        {
+            C1VelChanges.Add(0);
+            C2VelChanges.Add(0);
+        }
+    }
+    
+    private void FixedUpdate()
+    {
+        StickReadOut();
+        //Debug.Log($"C1 {smoothedC1VelocityChanges}");
+        ShakeState c1Shake = EvaluateCurrentShake(smoothedC1VelocityChanges);
+        RegisterShakes(c1Shake, ref C1_lastBiggestShake, Cowboy.Cowboy1);
+        
+        ShakeState c2Shake = EvaluateCurrentShake(smoothedC2VelocityChanges);
+        RegisterShakes(c2Shake, ref C1_lastBiggestShake, Cowboy.Cowboy2);
     }
 
-    // Update is called once per frame
-    void Update()
+    //Tracks the veloctiy of the sticks and feeds the class velocity values.
+    private void StickReadOut()
     {
         Vector2 Cowboy_1_Stick___Value = Cowboy_1_Stick.ReadValue<Vector2>();
         Vector2 Cowboy_2_Stick___Value = Cowboy_2_Stick.ReadValue<Vector2>();
@@ -27,12 +76,81 @@ public class BottleShaker : MonoBehaviour
         float C1delta = (Cowboy_1_Stick___Value - lastC1Value).magnitude;
         float C2delta = (Cowboy_2_Stick___Value - lastC2Value).magnitude;
 
-        float C1Velocity = C1delta / Time.deltaTime;
-        float C2Velocity = C2delta / Time.deltaTime;
-        
-        Debug.Log($"Stick Left: {Cowboy_1_Stick___Value}, vel: {C1Velocity}   Stick Right: {Cowboy_2_Stick___Value}, vel: {C2Velocity} @ {Time.frameCount}");
+        float lastC1Velocity = C1Velocity;
+        float lastC2Velocity = C2Velocity;
 
+        C1Velocity = C1delta / Time.fixedDeltaTime;
+        C2Velocity = C2delta / Time.fixedDeltaTime;
+
+        float c1VelocityChange = Mathf.Abs(C1Velocity - lastC1Velocity);
+        C1VelChanges.Add(c1VelocityChange);
+        C1VelChanges.RemoveRange(0,1);
+        
+        float c2VelocityChange = Mathf.Abs(C2Velocity - lastC2Velocity);
+        C2VelChanges.Add(c2VelocityChange);
+        C2VelChanges.RemoveRange(0,1);
+
+        smoothedC1VelocityChanges = 0;
+        foreach (var entry in C1VelChanges)
+            smoothedC1VelocityChanges += entry;
+        smoothedC1VelocityChanges /= VelocityChangeSmoothing;
+        
+        smoothedC2VelocityChanges = 0;
+        foreach (var entry in C2VelChanges)
+            smoothedC2VelocityChanges += entry;
+        smoothedC2VelocityChanges /= VelocityChangeSmoothing;
+        
         lastC1Value = Cowboy_1_Stick___Value;
         lastC2Value = Cowboy_2_Stick___Value;
+    }
+    
+    private ShakeState EvaluateCurrentShake(float smoothedVelChange)
+    {
+        if (smoothedVelChange > BigShake)
+            return ShakeState.Big;
+        if (smoothedVelChange > MediumShake)
+            return ShakeState.Medium;
+        if (smoothedVelChange > SmallShake)
+            return ShakeState.Small;
+        
+        return ShakeState.Rest;
+    }
+
+    private void RegisterShakes(ShakeState currentShake, ref ShakeState highestShake, Cowboy cowboy)
+    {
+        switch (currentShake)
+        {
+            case ShakeState.Small:
+                if (highestShake < ShakeState.Small)
+                    highestShake = ShakeState.Small;
+                break;
+            
+            case ShakeState.Medium:
+                if (highestShake < ShakeState.Medium)
+                    highestShake = ShakeState.Medium;
+                break;
+            
+            case ShakeState.Big:
+                if (highestShake < ShakeState.Big)
+                    highestShake = ShakeState.Big;
+                break;
+            
+            default:
+            case ShakeState.Rest:
+                if (highestShake > ShakeState.Rest)
+                {
+                    Debug.Log($"SHAKE REGISTERED: {highestShake}");
+                    ShakeEvent?.Invoke(cowboy, highestShake);
+                    highestShake = ShakeState.Rest;
+                }
+                break;
+        }
+    }
+
+    
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(new Vector2(500,500), new Vector2(500,500)), smoothedC1VelocityChanges.ToString());
     }
 }
---
+++ Assets/GameManager.cs
@@ -0,0 +1,124 @@
+using System;
+using GGJ_Cowboys;
+using UnityEngine;
+using NaughtyAttributes;
+using UnityEngine.Serialization;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    
+    [SerializeField, ReadOnly, BoxGroup("GameInfo")]
+    private GameState currentGameState = GameState.Menu;
+    public GameState CurrentGameState
+    {
+        get => currentGameState;
+        set
+        {
+            if (value == currentGameState)
+            {
+                Debug.LogWarning($"Tried to set same GameState {value}. This shouldn't be happening.");
+                return;
+            }
+            SetGameState(value, currentGameState);
+            currentGameState = value;
+            
+        }
+    }
+    
+    [SerializeField, ReadOnly, BoxGroup("GameInfo")]
+    private Cowboy activeCowboy = Cowboy.None;
+    public Cowboy ActiveCowboy
+    {
+        get => activeCowboy;
+        set
+        {
+            if(value == activeCowboy) return;
+            activeCowboy = value;
+        }
+    }
+
+    
+    //######################################################################################
+    //#################################   Awake & Start   ##################################
+    //######################################################################################
+    private void Awake()
+    {
+        SetSingleton();
+    }
+
+    private void SetSingleton()
+    {
+        if (Instance)
+        {
+            Debug.LogWarning($"GameManager instance is already occupied. This instance will be deleted.");
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+        Debug.Log("GameManager instance was set.");
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    
+    //######################################################################################
+    //################################   Game Functions   ##################################
+    //######################################################################################
+
+    private void SetGameState(GameState newState, GameState oldState)
+    {
+        //on leaving state
+        switch (oldState)
+        {
+            case GameState.Menu:
+                break;
+            case GameState.PreGame:
+                break;
+            case GameState.InGame:
+                break;
+            case GameState.Paused:
+                Time.timeScale = 1;
+                break;
+            case GameState.PostGame:
+                break;
+        }
+        
+        //on entering state
+        switch (newState)
+        {
+            case GameState.Menu:
+                ActiveCowboy = Cowboy.None;
+                break;
+            case GameState.PreGame:
+                ActiveCowboy = Cowboy.Cowboy1;
+                break;
+            case GameState.InGame:
+                break;
+            case GameState.Paused:
+                Time.timeScale = 0;
+                break;
+            case GameState.PostGame:
+                ActiveCowboy = Cowboy.None;
+                break;
+        }
+    }
+    
+    //######################################################################################
+    //####################################   Utility   #####################################
+    //######################################################################################
+    
+
+    public enum GameState
+    {
+        Menu,
+        PreGame,
+        InGame,
+        Paused,
+        PostGame
+    }
+}
